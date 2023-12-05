d2ad05f2075416a7add7ed677a2abf88d9e262bd
Isabella <143600689+isabella960@users.noreply.github.com>
2023-12-05 18:22:54 +0000

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,27 @@
+    main()
+
+
+    return data
+if __name__ == "__main__":
+def generate_random_data():
+
+    for item in data:
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def main():
+
+        print(f"Random Number: {item}")
+def main():
+    main()
+    return data
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+
+
+def generate_random_data():
+    for item in data:
+import random
