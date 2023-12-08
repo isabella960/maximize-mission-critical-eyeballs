5bdfebdfa5a45a58b200c97d4b5496afb028f090
Isabella <143600689+isabella960@users.noreply.github.com>
2023-12-08 13:39:28 +0000

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,27 +1,67 @@
-    main()
+        }
+}
+    }
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+        for (int item : data) {
 
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        }
 
-    return data
-if __name__ == "__main__":
-def generate_random_data():
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;    for (let i = 0; i < 10; i++) {
+function main() {
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
 
+    }
+function generateRandomData() {
+    data.forEach(item => {
+    return data;
+
+}
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+main();
+    });
+}    main()
     for item in data:
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
     data = generate_random_data()
-def main():
+
 
         print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+    return data
+import random
 def main():
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    data = generate_random_data()
+
+    main()
+
+    for item in data:
+def main():
+import random
+        print(f"Random Number: {item}")
+    return data
+
+if __name__ == "__main__":import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def main():
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+    for item in data:
+def generate_random_data():
     main()
     return data
 if __name__ == "__main__":
-
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-
-
-def generate_random_data():
-    for item in data:
-import random
