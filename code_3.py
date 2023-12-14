6b696045533dfd3011fffa605a9b3949be07797e
Isabella <143600689+isabella960@users.noreply.github.com>
2023-12-14 04:00:37 +0000

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,16 @@
+}
+    const data = [];
+        console.log("Random Number: " + item);
+main();
+    return data;
+    });
+    const data = generateRandomData();
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+
+function main() {
+    }
