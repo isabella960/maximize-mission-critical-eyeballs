caaade9e141139f0b4ed2dd4e12cd4ebdfc50308
Isabella <143600689+isabella960@users.noreply.github.com>
2023-12-22 13:29:52 +0000

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,40 +1,13 @@
 
 
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    return data
+def main():
 def generate_random_data():
 import random
     for item in data:
-    main()
-def main():
-
-
-    return data
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-if __name__ == "__main__":
-if __name__ == "__main__":
     data = generate_random_data()
 
     main()
-        print(f"Random Number: {item}")
-    for item in data:
-import random
-    return data
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def main():
-def generate_random_data():import random
-if __name__ == "__main__":
-def generate_random_data():
-    main()
-        print(f"Random Number: {item}")
-
-    return data
-    for item in data:
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def main():
-
-    data = generate_random_data()
